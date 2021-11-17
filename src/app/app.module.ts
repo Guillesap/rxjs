3907de9f305e8dcb1a9e206965cde38155b538b3
WillyS <hsapag@hotmail.com>
2021-11-17 20:35:41 -0300

    [subiendo archivos]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EjemploUnoComponent } from './ejemplo-uno/ejemplo-uno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EjemploUnoComponent
   ],
   imports: [
     BrowserModule,
